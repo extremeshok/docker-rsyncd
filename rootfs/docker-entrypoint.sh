d2dbac471d2c43141346dd485887dda462e9c11c
eXtremeSHOK <admin@extremeshok.com>
2018-11-02 15:39:02 +0200

    override config

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -13,44 +13,43 @@ chmod 0400 /etc/rsyncd.secrets
 mkdir -p "${VOLUME}"
 chown -R "root:root" "$VOLUME"
 
-if [ ! -f /etc/rsyncd.conf ] ; then
-  cat <<EOF > /etc/rsyncd.conf
-  pid file = /var/run/rsyncd.pid
-  log file = /dev/stdout
-  use chroot = yes
-  uid = root
-  gid = root
-  timeout = 300
-  max connections = 10
-  port = 873
-  reverse lookup = no
-  transfer logging = yes
-  ignore nonreadable = yes
-  dont compress   = *.gz *.tgz *.zip *.z *.Z *.rpm *.deb *.bz2 *.7z *.7zip *.exe *.rar
-  hosts deny = *
-  hosts allow = ${ALLOW_IP}
-  read only = false
-  [NetBackup]
-      path = ${VOLUME}
-      comment = NetBackup ${USERNAME} directory
-      auth users = ${USERNAME}
-      secrets file = /etc/rsyncd.secrets
-  [data]
-      path = ${VOLUME}
-      comment = Data ${USERNAME} directory
-      auth users = ${USERNAME}
-      secrets file = /etc/rsyncd.secrets
-  [Backup]
-      path = ${VOLUME}
-      comment = Backup ${USERNAME} directory
-      auth users = ${USERNAME}
-      secrets file = /etc/rsyncd.secrets
-  [MyCloud]
-      path = ${VOLUME}
-      comment = MyCloud ${USERNAME} directory
-      auth users = ${USERNAME}
-      secrets file = /etc/rsyncd.secrets
+echo "Generating Config"
+cat <<EOF > /etc/rsyncd.conf
+pid file = /var/run/rsyncd.pid
+log file = /dev/stdout
+use chroot = yes
+uid = root
+gid = root
+timeout = 300
+max connections = 10
+port = 873
+reverse lookup = no
+transfer logging = yes
+ignore nonreadable = yes
+dont compress   = *.gz *.tgz *.zip *.z *.Z *.rpm *.deb *.bz2 *.7z *.7zip *.exe *.rar
+hosts deny = *
+hosts allow = ${ALLOW_IP}
+read only = false
+[NetBackup]
+    path = ${VOLUME}
+    comment = NetBackup ${USERNAME} directory
+    auth users = ${USERNAME}
+    secrets file = /etc/rsyncd.secrets
+[data]
+    path = ${VOLUME}
+    comment = Data ${USERNAME} directory
+    auth users = ${USERNAME}
+    secrets file = /etc/rsyncd.secrets
+[Backup]
+    path = ${VOLUME}
+    comment = Backup ${USERNAME} directory
+    auth users = ${USERNAME}
+    secrets file = /etc/rsyncd.secrets
+[MyCloud]
+    path = ${VOLUME}
+    comment = MyCloud ${USERNAME} directory
+    auth users = ${USERNAME}
+    secrets file = /etc/rsyncd.secrets
 EOF
-fi
 
 exec /usr/bin/rsync --no-detach --daemon --config /etc/rsyncd.conf "$@"
